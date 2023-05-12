4a46c551439ee809ea74ea036c1b30dd7315f996
Mahmut Ali Şahinkaya <mahmutalisahinkaya88@gmail.com>
2023-05-12 19:37:30 +0300

    react-weather-app

---
+++ frontend-intermediate/react/react-weather-app/src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <pre>
+        Created by <a href="https://github.com/MahmutAliSahinkaya">Mahmut Ali Şahinkaya</a>
+      </pre>
+    </div>
+  );
+}
+
+export default Footer;
