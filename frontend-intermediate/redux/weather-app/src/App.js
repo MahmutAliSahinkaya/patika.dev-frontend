cc61431f672127690364dff1939d9aa583c58c20
Mahmut Ali Şahinkaya <mahmutalisahinkaya88@gmail.com>
2023-05-13 18:17:42 +0300

    weather-app

---
+++ frontend-intermediate/redux/weather-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Card from './components/Card/Card';
+
+function App() {
+
+ 
+  return (
+    <div className="container app">
+    <Card/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend-intermediate/redux/weather-app/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {AiFillLinkedin} from 'react-icons/ai'
+import {AiFillGithub} from 'react-icons/ai'
+function Footer() {
+  return (
+    <div className='mt-5'>
+
+        </div>
+  )
+}
+
+export default Footer
